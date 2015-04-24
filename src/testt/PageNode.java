b14dcaf6d00f8e085396d084dddea604d34cb0aa
xhd0216 <xhd0216@gmail.com>
2015-04-23 21:16:58 -0400

    tested page node

--- src/testt/PageNode.java
+++ src/testt/PageNode.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
+   
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
